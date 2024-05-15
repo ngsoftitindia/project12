49f7ad49b805886da27fa6d1adecc10aff78b40c
smartitsoloutions <prasads128312@gmail.com~>
2024-05-15 12:14:57 +0530

    this is my day two update

--- registration.tf
+++ registration.tf
@@ -2,9 +2,8 @@ iterradform {
   required_providers {
   monitoring             = true
   vpc_security_group_ids = ["sg-ii088---0-080=8uioio"]
-  subnet_id              = "subnet-051c481a09e876207"
-
   tags = {
+    project     = Tesing
     Terraform   = "true"
     Environment = "devandtest"
     Enviranmnnt = testing
